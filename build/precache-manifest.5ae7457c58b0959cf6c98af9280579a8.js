55212d84c5f742bf53757ac7bddd6f008bc3732e
Dan Vo <danvo.vo@gmail.com>
2022-09-26 01:02:46 -0700

    add README documentation with images

---
+++ build/precache-manifest.5ae7457c58b0959cf6c98af9280579a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cadf450fea956e4000e0",
+    "url": "/static/css/main.c5ca190d.chunk.css"
+  },
+  {
+    "revision": "cadf450fea956e4000e0",
+    "url": "/static/js/main.cadf450f.chunk.js"
+  },
+  {
+    "revision": "fb9f798d027acd51ad74",
+    "url": "/static/css/1.5d3829e6.chunk.css"
+  },
+  {
+    "revision": "fb9f798d027acd51ad74",
+    "url": "/static/js/1.fb9f798d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a50e5b7341e709687cbe3ee61ddbf329",
+    "url": "/index.html"
+  }
+];
